334bfbdcf0ea90f145935fbb3e1d50d804e5f03f
Faris Srnja <faris.srnja2004@gmail.com>
2023-07-04 11:04:43 +0200

    Form and createPost client

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,38 +1,47 @@
-import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
-import { connectToDB } from "@utils/database";
-import User from "@models/user";
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
+import User from '@models/user';
+import { connectToDB } from '@utils/database';
 
-console.log({ clientId: process.env.GOOGLE_ID, clientSecret: process.env.GOOGLE_CLIENT_SECRET })
 const handler = NextAuth({
-  providers: [GoogleProvider({ clientId: process.env.GOOGLE_ID, clientSecret: process.env.GOOGLE_CLIENT_SECRET })],
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-        email: session.user.email
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     })
-    session.user.id = sessionUser._id.toString()
-    return session
-  },
-  async signIn({ profile }) {
-    try{
-        await connectToDB()
-        // check if user exsists
-        const userExsists = await User.findOne({email: profile.email})
-        if(!userExsists){
-            await User.create({
+  ],
+  callbacks: {
+    async session({ session }) {
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
+
+      return session;
+    },
+    async signIn({ account, profile, user, credentials }) {
+      try {
+        await connectToDB();
+
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
+
+        // if not, create a new document and save user in MongoDB
+        if (!userExists) {
+          await User.create({
             email: profile.email,
             username: profile.name.replace(" ", "").toLowerCase(),
-            image: profile.picture
-        })
-    }
-        // if not make new user
-        return true
-    } catch (err) {
-        console.log(err)
-        return false
-    }
-  },
-});
+            image: profile.picture,
+          });
+        }
 
-export {handler as GET, handler as POST}
+        return true
+      } catch (error) {
+        console.log("Error checking if user exists: ", error.message);
+        return false
+      }
+    },
+  }
+})
+
+export { handler as GET, handler as POST }
